43ea6c40c78cb516fbedd1b6c53cec9bd17537df
Adarshkumar17 <adarshkumaremp@gmail.com>
2019-03-26 04:36:10 -0700

    Initial

---
+++ src/main/java/com/dbs/training/demoproject/Calculator.java
@@ -0,0 +1,23 @@
+package com.dbs.training.demoproject;
+
+public class Calculator {
+	
+	public int
+	add(int a,int b) {
+		return a+b;
+	}
+	public int sub(int a,int b) {
+		
+			return a-b;
+		}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Calculator c = new Calculator();
+		c.add(5, 5);
+		c.sub(10, 5);
+	}
+	}
+	
+
